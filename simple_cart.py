482609648407c4818fbbe094bec9cd6795e192dd
statusfailed <sf@statusfailed.com>
2022-12-09 14:49:07 +0000

    readability of simple_* scripts

--- simple_cart.py
+++ simple_cart.py
@@ -4,7 +4,8 @@ import gymnasium as gym
 env = gym.make("CartPole-v1", render_mode="human")
 observation, info = env.reset()
 for _ in range(1000):
-    action = 1 if observation[0] < 0 else 0
+    cart_position = observation[0]
+    action = 1 if cart_position < 0 else 0
     observation, reward, terminated, truncated, info = env.step(action)
     if terminated or truncated:
         observation, info = env.reset()
--- simple_pole.py
+++ simple_pole.py
@@ -4,7 +4,8 @@ import gymnasium as gym
 env = gym.make("CartPole-v1", render_mode="human")
 observation, info = env.reset()
 for _ in range(1000):
-    action = 1 if observation[2] > 0 else 0
+    pole_angle = observation[2]
+    action = 1 if pole_angle > 0 else 0
     observation, reward, terminated, truncated, info = env.step(action)
     if terminated or truncated:
         observation, info = env.reset()
